fed007db41db6066f71a68df675f4dafe625a1eb
egor <regress12345@gmail.com>
2025-09-09 20:03:49 +0300

    merge

---
+++ Shop/main.py
@@ -0,0 +1,51 @@
+class MainMenu:
+
+
+    menu = [
+        {
+            'title': 'Все товары',
+            'func': "all_product"
+        },
+        {
+            'title': 'Создать товар',
+            'func': "create_product"
+        },
+        {
+            'title': 'Завершить работу',
+            'func': "exit"
+        },
+    ]
+
+
+    def __init__(self):
+        self.get_command()
+
+
+
+    def show_menu(self):
+        print("\n Главное меню: \n")
+        for i, item in enumerate(self.menu):
+            print(f"{i+1}) {item.get('title')}")
+
+
+    def get_command(self):
+        self.show_menu()
+        command = input("\nВыбери пункт меню:")
+        try:
+            eval('self.' + self.menu[int(command) - 1].get('func'))()
+        except Exception:
+            print("\nТакого пункта меню не существует")
+            self.get_command()
+
+
+    def Exit(self):
+        print("Завершение работы...")
+
+
+
+
+MainMenu()
+
+
+
+
---
+++ Shop/product.py
@@ -0,0 +1,22 @@
+import abc
+
+
+class(abc.ABC):
+    name = None
+    price = None
+    count = None #колличество
+
+
+    def __init__(self,**kwargs):
+        if kwargs.get('name') is not None:
+            self.name = kwargs.get('name')
+        if kwargs.get('price') is not None:
+            self.price = kwargs.get('price')
+        if kwargs.get('count') is not None:
+            self.count = kwargs.get('count')
+
+
+    def get_info(self): pass
+
+
+
